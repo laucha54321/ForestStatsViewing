0ffbf02fd9b81f24e4be2bc744333a7d168abb64
Laureano Oliva <65315757+laucha54321@users.noreply.github.com>
2021-03-28 22:45:28 -0400

    Add files via upload

--- ForestStatsViewing.py
+++ ForestStatsViewing.py
@@ -1,11 +1,14 @@
 import pandas
-import sys
 import re
-from datetime import datetime
+import sys
+import datetime
+
+
 def setup():
     pandas.set_option('display.max_rows',500)
     pandas.set_option('display.max_columns',10)
-    pandas.set_option('display.width',150)
+    pandas.set_option('display.width',150)  #setup sets up the display for the inforamtion
+
 def applyFilter():
     print("Names of rows:")
     for index, value in enumerate(col_names):
@@ -18,7 +21,8 @@ def applyFilter():
     elif col_types[name]==bool:    
         inp_bool = (col_names[name] + '==' + val)
         dfd = df.query(inp_bool)
-    print(dfd)
+    print(dfd)  #applyFilter applies the fiter you want and displays the information
+
 def appMain():
     for index, value in enumerate(options):
         print(index, value)
@@ -33,26 +37,40 @@ def appMain():
         changeSettings()
     elif val == 0:
         return
-    appMain()
+    appMain()   #appMain is the main menu of the app
+
 def duration():
     #Select the index of the Tree you want to check Duration
-    indexx = int(input("Enter index: "))
-    #here the code gets the Start Time and End Time from     
-    dfd = [df.loc[indexx,'Start Time'],df.loc[indexx,'End Time']]
-    #dfd new is the timelist in string format
-    dfd_new = [dfd[0].replace(' GMT-03:00','')]
-    dfd_new.append(dfd[1].replace(' GMT-03:00',''))
-    #time list has the time in datetime format
-    time_list = [datetime.strptime(dfd_new[0],'%c')]
-    time_list.append(datetime.strptime(dfd_new[1],'%c'))
-    #calculates the duration with the time list
-    duration = time_list[1]-time_list[0]
-    print(duration)
-#def dataframesetup():
-    #for row in df:
-        #datetime.strotime(df.loc[index,'Start Time'].replace(' GMT-03:00'), '%c')
-setup()
+    indexx = input("Enter index: ")
+    indexx = convertList(indexx)
+    #its important to make sure that the values are voids
+    dfd = []
+    durationLi = []
+    durationTime = datetime.timedelta()
 
+    #here the code loops through the indexx and finds the Start and End time
+    for i in range (0, len(indexx)):
+        dfd.append([datetime.datetime.strptime(df.loc[indexx[i],'Start Time'].replace(' GMT-03:00',''),'%c'),datetime.datetime.strptime(df.loc[indexx[i],'End Time'].replace(' GMT-03:00',''),'%c')])
+    
+    #here the code loops and calculates the duration of each element in the list ( start - end time)
+    for i in range (0,len(dfd)):
+        durationLi.append(dfd[i][1]-dfd[i][0])
+
+    #here the code sums the duration of all the elements in the list
+    for i in range (0,len(durationLi)):
+        durationTime += durationLi[i]
+
+    print(durationTime) #duration calculates the difference between starts and end times from the information csv
+
+def convertList(string):
+    #Convrts the input string to a list of int to use as an index
+    li = list(string.split(","))
+    li = list(map(int, li))
+    return li   #convert to list creates a list from the input string to a list of int that can be used as indexes
+
+
+
+setup()
 #ts = ['DiaDeLaSemana','Mes','DiaNumerico','Horario','ZonaHoraria','Año']
 col_types = [str,str,str,str,str,bool]
 col_names = ['Start Time','End Time','Tag','Note','Tree Type','Success']
